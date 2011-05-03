7bcd558372dc88dee877fb8110e6f2b484d08a72
Devin Banks <banksda@rose-hulman.edu>
2011-05-02 23:37:29 -0400

    finished with lab

--- ChatBot-Team18/chatbot.rb
+++ ChatBot-Team18/chatbot.rb
@@ -2,6 +2,8 @@ STDOUT.sync = true
 
 puts "What is your name?"
 
+STDOUT.sync = true
+
 name = gets.chomp
 
 STDOUT.sync = true
@@ -12,9 +14,11 @@ STDOUT.sync = true
 
 puts "1: Global Thermonuclear War, 2: Chess, 3: Checkers, 4: Go, 5: Tic Tac Toe"
 
+STDOUT.sync = true
+
 number = gets.chomp
 
-if number == 1
+if number == "1"
 	print "BOOM!"
 else
 	print "NO"
