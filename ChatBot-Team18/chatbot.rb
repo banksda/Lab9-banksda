5e2507cbeeefe0bc4480eb54a5af23fc4267229c
Devin Banks <banksda@rose-hulman.edu>
2011-05-02 23:27:15 -0400

    added new implementation:

--- ChatBot-Team18/chatbot.rb
+++ ChatBot-Team18/chatbot.rb
@@ -1,1 +1,21 @@
-puts 'What is your name?'
+STDOUT.sync = true
+
+puts "What is your name?"
+
+name = gets.chomp
+
+STDOUT.sync = true
+
+puts "Hello, #{name}"
+
+STDOUT.sync = true
+
+puts "1: Global Thermonuclear War, 2: Chess, 3: Checkers, 4: Go, 5: Tic Tac Toe"
+
+number = gets.chomp
+
+if number == 1
+	print "BOOM!"
+else
+	print "NO"
+end
